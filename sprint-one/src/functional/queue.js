a441d39583a66fb2f26978cd5f42abe4795b56fd
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 05:41:43 -0700

    added comments and changed some part of code to my style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,6 +6,8 @@ var Queue = function() {
   var size = 0;
   var headIndex = 0;
   var key = 0;
+  // key exists for dequeue
+
   // Implement the methods belowe 
 
   someInstance.enqueue = function(value) {
@@ -14,13 +16,29 @@ var Queue = function() {
   };
 
   someInstance.dequeue = function() {
-    if (size !== 0){
-      var firstValue = storage[headIndex];
+
+    // after enqueueing, strorage should look something like th is:
+    // {
+    //   0 : 'a',
+    //   1 : 'b',
+    //   2 : 'c',
+    //   ...
+    //   98: 'a',
+    //   99: 'b',
+    //   ...
+    // }
+    // we deque 0 : 'a', but if we then enqueue, if we use size, it will overwrite
+    // the last property than making a new one due to the decrease of the size
+    // so we have an independent varaible key that is independent to size
+    // when we dequeue we remove 0, 1, 2... and enqueue we add 97, 98, 99...
+
+    if (size) {
+      var firstPropertiesValue = storage[headIndex];
       delete storage[headIndex];
-      headIndex ++;
-      size --;
+      headIndex++;
+      size--;
       
-      return firstValue;
+      return firstPropertiesValue;
     }
   };
 
