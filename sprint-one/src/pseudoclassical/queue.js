0188ec267edbdbbc0fdf61cd2dd0693799f2feca
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 11:17:46 -0700

    finished pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  this.storage = {};
+  this.sizeOfQueue = 0;
+  this.headIndex = 0;
+  this.key = 0;
+
+
+
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.key++] = value;
+  this.sizeOfQueue++;
+}
 
+Queue.prototype.dequeue = function() {
+  
+  if (this.sizeOfQueue) {
+    var firstPropertiesValue = this.storage[this.headIndex];
+    delete this.storage[this.headIndex];
+    this.headIndex++;
+    this.sizeOfQueue--;
+
+    return firstPropertiesValue;
+  }
+}
+
+Queue.prototype.size = function() {
+  return this.sizeOfQueue;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.sizeOfStack = 0;
+
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.sizeOfStack++] = value;
+}
 
+Stack.prototype.pop = function() {
+
+  if (this.sizeOfStack) {
+    var lastPropertiesValue = this.storage[this.sizeOfStack - 1];
+    delete this.storage[this.sizeOfStack];
+    this.sizeOfStack--;
+    return lastPropertiesValue
+  }
+}
+
+Stack.prototype.size = function() {
+  return this.sizeOfStack;
+}
