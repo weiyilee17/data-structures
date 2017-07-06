61aaa8457345ce8a9e5406512a3a868300046e77
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 10:34:55 -0700

    finished prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+
+  someInstance.storage = {};
+  someInstance.sizeOfQueue = 0;
+  someInstance.headIndex = 0;
+  someInstance.key = 0;
+
+
+  return someInstance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.key++] = value;
+  this.sizeOfQueue++;
+}
 
+queueMethods.dequeue = function() {
+  if (this.sizeOfQueue) {
+    var firstPropertiesValue = this.storage[this.headIndex];
+    delete this.storage[this.headIndex];
+    this.headIndex++;
+    this.sizeOfQueue--;
+    return firstPropertiesValue;
+  }
+}
+
+queueMethods.size = function() {
+  return this.sizeOfQueue;
+}
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.sizeOfStack = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.sizeOfStack++] = value;
+}
 
+stackMethods.pop = function () {
+  if (this.sizeOfStack) {
+    var lastPropertiesValue = this.storage[this.sizeOfStack - 1];
+    delete this.storage[this.sizeOfStack - 1];
+    this.sizeOfStack--;
+    return lastPropertiesValue;
+  }
+}
+
+stackMethods.size = function () {
+  return this.sizeOfStack;
+}
