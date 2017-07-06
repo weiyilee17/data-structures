c72ebb7976680a7352e14f03418406f63f58a385
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 07:37:48 -0700

    transfered from function to functional share

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.sizeOfStack = 0;
+  
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.sizeOfStack++] = value;
+}
 
+stackMethods.pop = function() {
+  if (this.sizeOfStack) {
+    var lastPropertiesValue = this.storage[this.sizeOfStack - 1];
+    delete this.storage[this.sizeOfStack - 1];
+    this.sizeOfStack--;
+    return lastPropertiesValue;
+  }
+}
+
+stackMethods.size = function() {
+  //console.log(this);
+  return this.sizeOfStack;
+}
