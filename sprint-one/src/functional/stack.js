fdd718367e0714dceaabeeed5dd5160f2f201528
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 05:40:21 -0700

    stopped the attempt to not use key and went back to the version with key

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,7 +3,6 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var keys = Object.keys(storage);  
   var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
@@ -11,17 +10,17 @@ var Stack = function() {
   };
   
   someInstance.pop = function() {
-    if (size !== 0){
-         var lastPropsValue = storage[size-1];
-         delete storage[size];
-         size--;
-         return lastPropsValue;
-       }
+    // only pop when object has properties
+    if (size){
+      var lastPropertiesValue = storage[size - 1];
+      delete storage[size];
+      size--;
+      return lastPropertiesValue;
+    }
   };
 
   someInstance.size = function() {
     return size;
-  
   };
 
   return someInstance;
