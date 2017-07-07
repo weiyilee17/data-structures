75f9ac82f76e67ee49be00915db2088f3fe1d50a
weiyilee17 <weiyilee17@gmail.com>
2017-07-06 23:15:39 -0700

    answered the complexity questions on comment

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -64,9 +64,13 @@ methods.depthFirstLog = function(iterator) {
    if(this.right) {
       this.right.depthFirstLog(iterator);
    }
+
 }
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert : O(log(n))
+ * contains : O(log(n))
+ * depthFirstLog : O(log(n))
  */
