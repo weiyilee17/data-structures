ca5d38d4323ae5119e81c5706d585c9acc0d4027
Hack Reactor Student <student@hackreactor.com>
2017-07-08 14:10:49 -0700

    added function getElementsBetweenIndex

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,4 +1,4 @@
-var linkedListNode = function (value) { // had to name it a different name to differentiate to graph's node
+var LinkedListNode = function (value) {
   var node = {};
 
   node.value = value;
@@ -11,48 +11,73 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.allNodeValues = [];  // array contains values of linkedList
 
   list.addToTail = function(value) {
 
     if (this.head === null) { // if list is empty
-      var newNode = linkedListNode(value);
+      var newNode = LinkedListNode(value);
       this.head = newNode;
       this.tail = newNode;
+      this.allNodeValues.push(newNode.value);
     } else {
       var prevTail = this.tail;
-      this.tail = linkedListNode(value);
+      this.tail = LinkedListNode(value);
+      this.allNodeValues.push(this.tail.value);
       prevTail.next = this.tail;
     }
-    
+
   };
 
   list.removeHead = function() {
     if (this.head === null) { // if list is empty
 
-    } else {
+    } else {  // else if the list isn't empty
       var prevHead = this.head;
       this.head = this.head.next;
+      this.allNodeValues.shift(); // get rid of the first element in the array (remove head)
       return prevHead.value;
     }
   };
 
   list.contains = function(target) {
 
-    var currentNode;
+    // after allNodeValues
 
-    for(currentNode = this.head; currentNode !== this.tail; currentNode = currentNode.next) {
-      if (currentNode.value === target) {
+    for(var i = 0; i < this.allNodeValues.length; i++) {
+      if (this.allNodeValues[i] === target){
         return true;
       }
     }
 
-    if (currentNode.value === target) { // for the last node in list
-      return true;
+    return false;
+
+    // before adding allNodeValues
+
+    // var currentNode;
+    //
+    // for(currentNode = this.head; currentNode !== this.tail; currentNode = currentNode.next) {
+    //   if (currentNode.value === target) {
+    //     return true;
+    //   }
+    // }
+    //
+    // if (currentNode.value === target) { // for the last node in list
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  };
+
+  list.getElementsBetweenIndex = function (startIndex, endIndex) {
+
+    if (startIndex === endIndex) {
+      return [];  // slice(0, 0 + 1) would return an array with the first element
     } else {
-      return false;
+      return this.allNodeValues.slice(startIndex, endIndex + 1);
     }
 
-  };
+  }
 
   return list;
 };
