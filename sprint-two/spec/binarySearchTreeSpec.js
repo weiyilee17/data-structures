f74228a13f5af0a8382768366bbff62df6c87b0c
Hack Reactor Student <student@hackreactor.com>
2017-07-08 14:21:32 -0700

    added min and max's test

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -36,4 +36,72 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3]);
   });
+
+  it('should execute a callback on every value in a more complicated tree using "depthFirstLog"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    expect(array).to.eql([5, 2, 1, 3, 4, 6, 9, 7, 8]);
+  });
+
+  it('should get minimum value of the tree"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    expect(binarySearchTree.min()).to.equal(1);
+  });
+
+  it('should get minimum value of an unballanced tree"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(8);
+    binarySearchTree.depthFirstLog(func);
+    expect(binarySearchTree.min()).to.equal(5);
+  });
+
+  it('should get maximum value of the tree"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(9);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    expect(binarySearchTree.max()).to.equal(9);
+  });
+
+  it('should get maximum value of an unballanced tree"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    expect(binarySearchTree.max()).to.equal(5);
+  });
+
 });
