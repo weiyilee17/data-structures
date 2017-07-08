ea3c858279d3b378d7a2e0e52cb3f449c93e1f07
Hack Reactor Student <student@hackreactor.com>
2017-07-08 14:13:04 -0700

    added min and max functions

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -20,7 +20,7 @@ methods.insert = function(newValue) {
       } else {
          this.left = BinarySearchTree(newValue);
       }
-      
+
    } else { // new value > current parrent node's value
       if(this.right) {
          return this.right.insert(newValue);
@@ -36,15 +36,15 @@ methods.contains = function(target) {
    if(target === this.value) {
       return true;
    } else if(target < this.value) {
-      
+
       if(this.left) {
          return this.left.contains(target);
       } else {
          return false;
       }
-      
+
    } else { // target > this.value
-      
+
       if(this.right) {
          return this.right.contains(target);
       } else {
@@ -54,6 +54,8 @@ methods.contains = function(target) {
 };
 
 methods.depthFirstLog = function(iterator) {
+
+  // pre order : parent, left, right
 
    iterator(this.value);
 
@@ -67,7 +69,25 @@ methods.depthFirstLog = function(iterator) {
 
 }
 
+methods.min = function () {
 
+  if(this.left) {
+    return this.left.min();
+  } else {
+    return this.value;
+  }
+
+}
+
+methods.max = function () {
+
+  if(this.right) {
+    return this.right.max();
+  } else {
+    return this.value;
+  }
+
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  * insert : O(log(n))
