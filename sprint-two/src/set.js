98d1d42c1f71c177c256ac861714332077155dc9
weiyilee17 <weiyilee17@gmail.com>
2017-07-09 21:46:05 -0700

    answered complexity questions and refactored it to prototypal

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,12 +1,12 @@
 var Set = function() {
-  var set = Object.create(Set.prototype);
+  var set = Object.create(Set);
   set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-Set.prototype.add = function(item) {
+Set.add = function(item) {
   if (this.contains(item)) { // if string is already in set, do nothing
 
   } else {  // only add when it is unique
@@ -14,7 +14,7 @@ Set.prototype.add = function(item) {
   }
 };
 
-Set.prototype.contains = function(item) {
+Set.contains = function(item) {
   for (var i = 0; i < this._storage.length; i++) {
     if (this._storage[i] === item) {
       return true;
@@ -23,7 +23,7 @@ Set.prototype.contains = function(item) {
   return false;
 };
 
-Set.prototype.remove = function(item) {
+Set.remove = function(item) {
   for (var i = 0; i < this._storage.length; i++) {
     if (this._storage[i] === item) {
       this._storage.splice(i, 1);
@@ -31,10 +31,14 @@ Set.prototype.remove = function(item) {
   }
 };
 
-Set.prototype.getLength = function() {
+Set.getLength = function() {
   return this._storage.length;
 }
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add : O(n)
+ * contains : O(n)
+ * remove : O(n)
+ * getLength : O(1)
  */
