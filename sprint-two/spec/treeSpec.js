2d55d6138edd9c0d3d5b7bebf6a8d861224a7721
weiyilee17 <weiyilee17@gmail.com>
2017-07-09 22:47:16 -0700

    added test cases for getElementCount

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,12 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should correctly return element count', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    expect(tree.getElementCount()).to.equal(4);
+  });
+
 });
